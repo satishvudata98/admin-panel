c624258d338002ceab06a5fed00e5fe39e39fc27
satishvudata98 <satishvudata98@gmail.com>
2025-05-23 01:45:36 +0530

    supabase client connnection logic update

--- backend/config/supabaseClient.js
+++ backend/config/supabaseClient.js
@@ -1,8 +1,10 @@
-import { createClient } from '@supabase/supabase-js';
+require('dotenv').config();
+const { createClient } = require('@supabase/supabase-js');
 
 const supabaseUrl = 'https://huqsmrocdnzhswwqpeeh.supabase.co';
 const supabaseKey = process.env.SUPABASE_API_KEY;
 
 const supabase = createClient(supabaseUrl, supabaseKey);
 
-export default supabase;
+module.exports = supabase;
+
